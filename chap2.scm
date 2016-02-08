30d451076ebb0391d23722968ff64a0b3b37cc86
gurvesh <guru01@Fox17com.local>
2016-02-08 20:35:28 +0000

    Chap 2 still on

--- chap2.scm
+++ chap2.scm
@@ -618,3 +618,52 @@
         ans
         (iter-range (- n 1) (cons n ans))))
   (iter-range (- n 1) nil))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (range-interval low high)
+  (if (> low high)
+      nil
+      (cons low (range-interval (+ low 1) high))))
+
+;; Ex 2.33 ;;
+
+(define (__map p sequence)
+  (accumulate (lambda (x y)
+                (cons (p x) y))
+              nil
+              sequence))
+
+(define (_append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (_length sequence)
+  (accumulate (lambda (x y)
+                (+ 1 y))
+              0 sequence))
+
+;; Ex 2.34 ;;
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ this-coeff
+                   (* x higher-terms)))
+              0
+              coefficient-sequence))
+
+;; Ex 2.35 ;;
+
+(define (_count-leaves t)
+  (accumulate + 0 (map (lambda (x) 1)
+                       t)))
