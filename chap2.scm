ea23c9e0dcade5622846f94ea75bbc17e515a260
gurvesh <guru01@gmail.com>
2016-02-10 19:55:59 +0000

    Chap 2

--- chap2.scm
+++ chap2.scm
@@ -710,7 +710,7 @@
 
 ;; scheme@(guile-user)> (fold-left / 1 (list 1 2 3))
 ;; $39 = 1/6
-;; scheme@(guile-user)> (accumulate / 1 (list 1 2 3))
+;; scheme@(guile-user)> (fold-right / 1 (list 1 2 3))
 ;; $40 = 3/2
 
 ;; scheme@(guile-user)> (fold-right list nil (list 1 2 3))
@@ -734,3 +734,69 @@
   (fold-left (lambda (x y)
                (cons y x))
              nil sequence))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (accumulate append
+;;             nil
+;;             (map (lambda (i)
+;;                    (map (lambda (j) (list i j))
+;;                         (range-interval 1 (- i 1))))
+;;                  (range-interval 1 n)))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+;; This is equivalent to mapcat in clojure.
+
+(define mapcat flatmap)
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(define true #t)
+(define false #f)
+
+(define (make-pair-sum pair)
+  (append pair (list (+ (car pair)(cadr pair)))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (flatmap
+                (lambda (i)
+                  (map (lambda (j) (list i j))
+                       (range-interval 1 (- i 1))))
+                (range-interval 1 n)))))
