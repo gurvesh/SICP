3c8d908286cc4a18819af9c9369ae6cb992be1dc
Gurvesh <guru01@gmail.com>
2016-02-29 16:31:12 +0000

    Sets

--- chap2.scm
+++ chap2.scm
@@ -1243,3 +1243,86 @@
                                          (_multiplicand exp))))
         (else
          (error "unknown expression type: DERIV" exp))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Sets
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+;;;;;;;;;;;;;
+;; Ex 2.59 ;;
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2))
+        (else (cons (car set1) (union-set (cdr set1) set2)))))
+
+;;;;;;;;;;;;;
+;; Ex 2.60 ;;
+
+(define (_adjoin-set x set)
+  (cons x set))
+
+(define (_union-set set1 set2)
+  (append set1 set2))
+
+;; The above two procedures are simpler. Intersection and element-of will not be any easier 
+
+;;;;;;;;;;;;;;;;;;;;;
+;; Sets as ordered list ;;;
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1))
+            (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1 (intersection-set (cdr set1)
+                                          (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
+
+;;;;;;;;;;;;;
+;; Ex 2.61 ;;
+
+(define (adjoin-set x set)
+  (cond ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (let ((x1 (car set1))
+                    (x2 (car set2)))
+                (cond ((< x1 x2)
+                       (cons x1 (union-set (cdr set1) set2)))
+                      ((> x1 x2)
+                       (cons x2 (union-set set1 (cdr set2))))
+                      ((= x1 x2)
+                       (cons x1 (union-set (cdr set1) (cdr set2)))))))))
+
