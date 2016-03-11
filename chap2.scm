d42b8abe15a5d3677778d415fbfa2c21232be3db
Gurvesh <guru01@gmail.com>
2016-03-11 10:34:58 +0000

    Huffman

--- chap2.scm
+++ chap2.scm
@@ -1420,3 +1420,77 @@
 ;;;;;;;;;;;;;
 ;; Ex 2.65 ;;
 
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Huffman encoding ;;;;;;;;;
+
+(define (make-leaf symbol weight) (list 'leaf symbol weight))
+(define (leaf? object) (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch-huff tree) (car tree))
+(define (right-branch-huff tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+;; Decoding procedure
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) next-branch))))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch-huff branch))
+        ((= bit 1) (right-branch-huff branch))
+        (else (error "bad bit: CHOOSE-BRANCH" bit))))
+
+;; Adding a new element to a set already weighted
+
+(define (adjoin-set-huff x set)
+  (cond ((null? set) (list set))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set-huff x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set-huff (make-leaf (car pair)  ;; symbol
+                                    (cdr pair)) ;; frequency
+                         (make-leaf-set (cdr pairs))))))
+
+;;;;;;;;;;;;;
+;; Ex 2.67 ;;
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree
+                    (make-leaf 'D 1)
+                    (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
