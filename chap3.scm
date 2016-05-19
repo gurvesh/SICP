acba6c5be439016e91f69b7206cc3649c7eee358
Gurvesh <guru01@gmail.com>
2016-05-19 18:32:03 +0100

    Part 3.1.1 complete

--- chap3.scm
+++ chap3.scm
@@ -21,3 +21,42 @@
           (else (error "Unknown request: MAKE-ACCOUNT"
                        m))))
   dispatch)
+
+;;;;;;;;;;;;
+;; Ex 3.1 ;;
+
+(define (make-accumulator sum)
+  (lambda (amount)
+    (begin (set! sum (+ sum amount))
+           sum)))
+
+;;;;;;;;;;;;
+;; Ex 3.2 ;;
+
+(define (make-monitored proc)
+  (let ((calls 0))
+    (lambda (arg)
+      (if (eq? arg 'how-many-calls?)
+          calls
+          (begin (set! calls (+ calls 1))
+                 (proc arg))))))
+
+;;;;;;;;;;;;
+;; Ex 3.3 ;;
+
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficint funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch password-entered m)
+    (cond ((not (eq? password-entered password)) "Incorrect password")
+          ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request: MAKE-ACCOUNT"
+                       m))))
+  dispatch)
