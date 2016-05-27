6e35ffbe8f26b7b7bb4a639bd56769617ec33e47
Gurvesh <guru01@gmail.com>
2016-05-27 16:16:36 +0100

    updates rand function

--- chap3.scm
+++ chap3.scm
@@ -107,6 +107,9 @@
 ;;;;;;;;;;;;
 ;; Ex 3.5 ;;
 
+(define (random range)
+  (integer (* range (/ (rand)(- (expt 2 31) 1)))))
+
 (define (random-in-range low high)
   (let ((range (- high low)))
     (+ low (random range))))
