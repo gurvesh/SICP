51d31c2f7877af57c0f8f8072e0a5dada25f1a18
gurvesh <guru01@gmail.com>
2016-08-15 06:32:48 +0100

    Starts the part on electrical circuits

--- chap3.scm
+++ chap3.scm
@@ -692,3 +692,99 @@
            ((= n 1) 1)
            (else (+ (memo-fib (- n 1))
                     (memo-fib (- n 2))))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Electrical circuits ;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (half-adder a b s c)
+  (let ((d (make-wire))
+        (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-get d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-proc)
+    (let ((new-value (logical-and (get-signal a1)
+                                  (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-proc)
+  (add-action! a2 and-action-proc)
+  'ok)
+
+(define (valid-signal? s)
+  (and (number? s)
+       (or (= s 0)
+           (= s 1))))
+
+(define (check-signals . sigs)
+  (cond ((null? sigs) #t)
+        ((valid-signal? (car sigs))
+         (apply check-signals (cdr sigs)))
+        (else #f)))
+
+(define (logical-and s1 s2)
+  (if (check-signals s1 s2)
+      (if (and (= s1 1) (= s2 1))
+          1
+          0)
+      (error "Invalid signal" s1 s2)))
+
+;;;;;;;;;;;;;
+;; Ex 3.28 ;;
+
+(define (or-gate a1 a2 output)
+  (define (or-action-proc)
+    (let ((new-value (logical-or (get-signal a1)
+                                 (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-proc)
+  (add-action! a2 or-action-proc)
+  'ok)
+
+(define (logical-or s1 s2)
+  (if (check-signals s1 s2)
+      (if (or (= s1 1) (= s2 1))
+          1
+          0)
+      (error "Invalid signal s1 s2")))
+
+;;;;;;;;;;;;;
+;; Ex 3.29 ;;
+
+(define (_or-gate a1 a2 output)
+  (let ((s (make-wire)))
+    (and-gate a1 a2 s)
+    (not-gate s output)
+    'ok))
+
+;; Delay time should be additive
