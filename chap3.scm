4f3fb346a48f343df797be225eee14ae6d1fca1f
Gurvesh <guru01@gmail.com>
2016-09-16 15:51:49 +0100

    Continues streams

--- chap3.scm
+++ chap3.scm
@@ -1378,3 +1378,86 @@ The above code will not run until the mutex is released
           (else (iter (+ count 1) accum))))
   (iter a 0))
 
+;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s)
+		  (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+		   (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+	     (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (stream-range low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream low
+		   (stream-range (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+	((pred (stream-car stream))
+	 (cons-stream (stream-car stream)
+		      (stream-filter pred
+				     (stream-cdr stream))))
+	(else (stream-filter pred (stream-cdr stream)))))
+
+;;;
+(define (range low high)
+  (if (> low high)
+      '()
+      (cons low (range (+ low 1) high))))
+;;;
+
+(define (memo-proc proc)
+  (let ((already-run? #f)
+	(result #f))
+    (lambda ()
+      (if (not already-run?)
+	  (begin (set! result (proc))
+		 (set! already-run? #t)
+		 result)
+	  result))))
+
+(define (delay proc)
+  (memo-proc (lambda ()
+	       proc)))
+
+(define (force delayed-object)
+  (delayed-object))
+
+;;;;;;;;;;;;;
+;; Ex 3.50 ;;
+
+(define (stream-map proc . argstreams)
+  (if (null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map proc (map stream-cdr argstreams)))))
+
+;;;;;;;;;;;;;
+;; Ex 3.51 ;;
+
+(define (show x)
+  (display-line x)
+  x)
+
